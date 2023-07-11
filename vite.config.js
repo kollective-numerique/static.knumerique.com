b5db16176e6086aa8e82b4ab58a54c24df667aec
seleshabani <seleshabani4@gmail.com>
2023-07-12 00:14:43 +0100

    main : responsive mobile & tablette & vite config for multipage build

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,16 @@ const path = require('path')
 export default {
   root: path.resolve(__dirname, 'src'),
   build: {
-    outDir: '../dist'
+    outDir: '../dist',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        agence: resolve(__dirname, 'src/agence.html'),
+        apropos: resolve(__dirname, 'src/apropos.html'),
+        blog: resolve(__dirname, 'src/blog.html'),
+        academie: resolve(__dirname, 'src/academie.html'),
+      },
+    },
   },
   server: {
     port: 8080
